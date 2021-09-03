7c272c56703221062da9a4354b68b0e23977b9e9
moses1418 <mosesrodriguez1414@yahoo.com>
2021-09-03 17:15:10 -0500

    updates to triphub

---
+++ src/main/resources/static/js/views/home.js
@@ -0,0 +1,16 @@
+export default function Home(props) {
+    console.log("The frontend did it. HER FAULT");
+    return `
+    <div class="container border shadow">
+        <header>
+            <h1>Home Page</h1>
+        </header>
+        <main>
+            <div>
+                <p>
+                    This is the home page text.
+                </p>    
+            </div>
+        </main>
+    </div>`;
+}
---
+++ src/main/resources/static/js/views/partials/navbar.js
@@ -0,0 +1,25 @@
+export default function Navbar(props){
+    return `<nav class="navbar navbar-expand sticky-top navbar-dark bg-dark">
+        <a class="navbar-brand p-1" href="#">TripHub</a>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a href="/" data-link class="p-2" style="text-decoration:none; color:white">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a href="/posts" data-link class="p-2" style="text-decoration:none; color:white">Posts</a>
+            </li>
+            <li class="nav-item">
+                <a href="/about" data-link class="p-2" style="text-decoration:none; color:white">About</a>
+            </li>
+            <li class="nav-item">
+                <a href="/login" data-link class="p-2" style="text-decoration:none; color:white">Login</a>
+            </li>
+            <li class="nav-item">
+                <a href="/users" data-link class="p-2" style="text-decoration:none; color:white">Register</a>
+            </li>
+            <li class="nav-item">
+                <a href="/search" data-link class="p-2" style="text-decoration:none; color:white">Search</a>
+            </li>
+        </ul>
+</nav>`
+}
