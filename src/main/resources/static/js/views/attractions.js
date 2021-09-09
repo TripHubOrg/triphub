ebbae7ecb46d1ea031f0b3555769681f4dd0cf97
moses1418 <mosesrodriguez1414@yahoo.com>
2021-09-09 10:42:50 -0500

    adding list for attraction.js

--- src/main/resources/static/js/views/attractions.js
+++ src/main/resources/static/js/views/attractions.js
@@ -1,4 +1,8 @@
 import * as KEYS from "../keys.js"
+import createView from "../createView.js";
+
+
+var attractionsArray;
 
 export function attractionsRequest(coordinates) {
     console.log(coordinates)
@@ -9,25 +13,42 @@ export function attractionsRequest(coordinates) {
         }
     }).then(response => {
         response.json().then( data => {
-            console.log(data)
+            console.log(data.features)
+           attractionsArray = data.features;
+            createView("/attractions")
+
         })
     })
-
-
-    // $.ajax({
-    //     url: "https://opentripmap.io/openapi.en.json",
-    //     type: "GET",
-    //     data: {
-    //         APPID: OPEN_TRIPMAP_KEY,
-    //         lat: coordinates[1],
-    //         lon: coordinates[0],
-    //     },
-    //     success: function (data) {
-    //
-    //         howItIsDisplayed(myLoopFunction(data))
-    //     }
-    // });
 }
+
+    export default function AttractionsView(props){
+        return   `<div class="container border shadow" id="attractionsPage">
+                    <header>
+                        <h1>Attractions</h1>
+                    </header>
+                    <main>
+                        <div id="attractionsList"></div>
+                        <div id="endOfList">End of list</div>
+                    </main>
+                </div>`
+    }
+
+    export function BeginAttractionsEvents(){
+        renderAttractions(attractionsArray)
+    }
+
+    function renderAttractions(listOfAttractions){
+        console.log(listOfAttractions)
+        $("#attractionsList").append(`
+        ${listOfAttractions.map(attraction => `
+        <div>
+            <div>${attraction.properties.name}</div>
+        </div>
+        `).join('')}
+        `)
+
+    }
+
 
 function myLoopFunction(features) {
     console.log(features)
@@ -89,40 +110,41 @@ from /xid
     return events
 }
 
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//***** LOAD MORE ATTRACTIONS FUNCTIONALITY ******
-let attractionsContainer;
-let endOfListDiv;
-
-
-//Will need 2 new variables to setup IntersectionObserver -> "option" & "callback"
-
-let option = {
-    root: null, //This is the viewport, if set to null then it will use the entire document as like the screen to monitor our callback function later
-    rootMargin: '0px', // like in CSS, you can set a margin amount, basically reducing viewport size
-    threshold: 1.0 //Goes from 0 -> 1, this is how much of your 'target' (in this case the last div tag for attractions) needs to be seen in viewport to execute the callback function
-}
-
-//callback needs both of these parameters but we will only be using entry
-let callback = function(entries, observer){
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            //fetch request functions and other stuff would go here
-            //if we put a div at the very end of our page, once the DOM sees that last div when we scroll, it will execute any code in here
-        }
-    })
-}
-
-//Lastly, need to instantiate a new IntersectionObserver object, like in  Java
-let observer = new IntersectionObserver(callback, option)
-//and set target, meaning what the observer will observe for executing callback
-endOfListDiv = document.getElementById('some-div') //will replace this later
-observer.observe(endOfListDiv)
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*****
-
-
-function toggleBTN(){
-    $("#toggleBTN").click(function () {
-        window.location.href = "http://http://localhost:8080/mapbox";
-    })
-}
+// //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// //***** LOAD MORE ATTRACTIONS FUNCTIONALITY ******
+// let attractionsContainer;
+// let endOfListDiv;
+//
+//
+// //Will need 2 new variables to setup IntersectionObserver -> "option" & "callback"
+//
+// let option = {
+//     root: null, //This is the viewport, if set to null then it will use the entire document as like the screen to monitor our callback function later
+//     rootMargin: '0px', // like in CSS, you can set a margin amount, basically reducing viewport size
+//     threshold: 1.0 //Goes from 0 -> 1, this is how much of your 'target' (in this case the last div tag for attractions) needs to be seen in viewport to execute the callback function
+// }
+//
+// //callback needs both of these parameters but we will only be using entry
+// let callback = function(entries, observer){
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting){
+//             //fetch request functions and other stuff would go here
+//             //if we put a div at the very end of our page, once the DOM sees that last div when we scroll, it will execute any code in here
+//         }
+//     })
+// }
+//
+// //Lastly, need to instantiate a new IntersectionObserver object, like in  Java
+// let observer = new IntersectionObserver(callback, option)
+// //and set target, meaning what the observer will observe for executing callback
+// endOfListDiv = document.getElementById('some-div') //will replace this later
+// observer.observe(endOfListDiv)
+// //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*****
+//
+//
+// // function toggleBTN(){
+// //     $("#toggleBTN").click(function () {
+// //         window.location.href = "http://http://localhost:8080/mapbox";
+// //     })
+// // }
+// }
