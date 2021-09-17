1fd4b14365961da5f4e872a85004aaf213e7e3fe
NathanRodriguez04 <naterod26@gmail.com>
2021-09-17 11:16:36 -0500

    Trips

--- src/main/java/com/codeup/capstonestarter/web/TripsController.java
+++ src/main/java/com/codeup/capstonestarter/web/TripsController.java
@@ -1,16 +1,19 @@
-package com.codeup.capstonestarter.web;public class TripsController {
 package com.codeup.capstonestarter.web;
 import com.codeup.capstonestarter.data.trip.Trip;
 import org.springframework.web.bind.annotation.*;
 
-    @RestController
-    @RequestMapping(value="/api/trips",headers = "Accept=applications/json")
-    public class TripsController {
+@RestController
+@RequestMapping(value="/api/trips",headers = "Accept=applications/json")
+public class TripsController {
+
         @PutMapping("{id}")
-        private void editTrips getPosts(@PathVariable Long id, @RequestBody Trip trip) {
-            System.out.println(trip.getClass());
+        private void editTrips (@PathVariable Long id, @RequestBody Trip trip) {
+            System.out.println(trip.getStarDate());
         }
-        private void editTrips getPosts() {return  }
+    @PutMapping("{/{id}}")
+        private void updateTrips(@PathVariable Long id,@RequestBody Trip whateveriwant) {
+        System.out.println(whateveriwant.getUsers());
+    }
 
 
     }
