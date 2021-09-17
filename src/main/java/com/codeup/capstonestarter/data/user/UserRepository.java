dfc740e2662a1009ca3e1af86009549cd4ae4e0c
darkmageman4 <darkmageman4@yahoo.com>
2021-09-17 17:38:44 -0400

    security Implementation

--- src/main/java/com/codeup/capstonestarter/data/user/UserRepository.java
+++ src/main/java/com/codeup/capstonestarter/data/user/UserRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<Object> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
