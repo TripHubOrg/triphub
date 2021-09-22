50c4278e2e1911a086538f15ac283eac284c0872
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-09-21 19:17:40 -0600

    working on styling

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -2,17 +2,16 @@ import Mapbox from "../mapbox.js";
 
 export default function Home(props) {
 	return `
-	<div class=" home-content container">
-		<div class="container-fluid">
-			<div class="row d-flex justify-center align-self-center my-5">
-				<div class="col col-md-12 " id="homepage">
+	<div class="home-content container vh-100 p-0">
+		<div class="container-fluid vh-100 p-0">
+			<div class="row d-flex justify-center align-self-center m-0 vh-100">
+				<div class="col col-md-12 vh-100 p-0" id="homepage">
 					<header>
-						<h1 class="d-flex justify-content-center mt-2 mb-2">PLAN A TRIP</h1>
+						<h1 class="d-flex justify-content-center">PLAN A TRIP</h1>
 					</header>
 					<div class="description d-flex justify-content-center">Search for a City...</div>
 					<div id="geocoder-container" class="d-flex justify-content-center mt-1 mb-5"></div>
 					<div id="map" style="visibility: collapse"></div>
-					<div class="bg-image" style="background-image: url('../static/assets/blonde-1867768_1920.jpg')"></div>
 <!--					<img src="../assets/blonde-1867768_1920.jpg" alt="" class="home-img">-->
 				</div>
 			</div>
