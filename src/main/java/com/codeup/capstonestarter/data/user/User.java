e1c79db8494042b1ace565c7631051c247b86531
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-09-27 08:34:11 -0600

    working on updating sprint-3

--- src/main/java/com/codeup/capstonestarter/data/user/User.java
+++ src/main/java/com/codeup/capstonestarter/data/user/User.java
@@ -1,6 +1,8 @@
 package com.codeup.capstonestarter.data.user;
 
 import com.codeup.capstonestarter.data.trip.Trip;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -24,48 +26,59 @@ public class User {
     @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false, length = 100)
+    @Column
     private String password;
 
-//    @Enumerated(EnumType.STRING)
-//    @Column(nullable = false)
-//    private Role role = Role.USER;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @JsonIgnore
+    private Role role = Role.USER;
 
+    public enum Role {USER, ADMIN,}
 
-
-    @ManyToOne (cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH})
-    private Trip trip;
-
-    @OneToMany
-    @JoinColumn()
+    @OneToMany(mappedBy = "owner")
+    @JsonIgnoreProperties("owner")
     private Collection<Trip> owned_trips;
 
-    @ManyToMany
-    @JoinColumn()
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {/*CascadeType.MERGE, */CascadeType.DETACH, CascadeType.REFRESH},
+            targetEntity = Trip.class
+    )
+    @JoinTable(
+            name="user_trip",
+            //What are your two foreign keys for your two tables
+            joinColumns = {@JoinColumn(name = "user_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "trip_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
     private Collection<Trip> collab_trips;
 
-    public User(String full_name, Long id, String username, String email, String password) {
-        this.full_name = full_name;
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-
-    }
 
     public User() {
     }
 
-    public String setfull_name() {
-        return full_name;
-    }
-
-    public void getfull_name(Long id) {
+    public User(String full_name, String username, String email, String password) {
         this.full_name = full_name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
     }
 
-    public User(String username) {
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public User(Long id, String full_name, String username, String email, String password, Role role, Collection<Trip> owned_trips, Collection<Trip> collab_trips) {
+        this.id = id;
+        this.full_name = full_name;
         this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.owned_trips = owned_trips;
+        this.collab_trips = collab_trips;
     }
 
     public Long getId() {
@@ -74,6 +87,14 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
     }
 
     public String getUsername() {
@@ -100,11 +121,19 @@ public class User {
         this.password = password;
     }
 
-//    public Role getRole() {
-//        return role;
-//    }
+    public Role getRole() {
+        return role;
+    }
 
-//    public void setRole(Role role) {
-//        this.role = role;
-//    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Collection<Trip> getOwned_trips() {
+        return owned_trips;
+    }
+
+    public void setOwned_trips(Collection<Trip> owned_trips) {
+        this.owned_trips = owned_trips;
+    }
 }
